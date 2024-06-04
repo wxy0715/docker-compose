f4a76d9239d9f278df1cc336bb04d940363f12ce
wxy <1743657883@qq.com>
2024-06-04 10:21:57 +0800

    1

--- system_upgrade_docker.sh
+++ system_upgrade_docker.sh
@@ -88,8 +88,8 @@ install_docker_compose() {
 
 # 完成
 install_docker_portainer() {
-  sudo docker pull portainer/portainer
-  docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/docker/portainer:/data portainer/portainer
+  sudo docker pull portainer/portainer-ce:2.19.5
+  docker run -d -p 9000:9000 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v    /mnt/docker/portainer:/data portainer/portainer-ce:2.19.5
   # docker restart portainer
   iptables -I INPUT -p tcp --dport 9000 -j ACCEPT
   service iptables save
